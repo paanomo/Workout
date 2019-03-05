9467028ef8a0ef359239c63031aea06400f3ea0c
paanomo <47227526+paanomo@users.noreply.github.com>
2019-03-04 22:51:01 -0800

    Initial commit

---
+++ app/src/main/java/com/example/workout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.workout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+        startActivity(intent);
+    }
+}
